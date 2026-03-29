d2426e0533fc9ecf0bf6c25fc3ddd0b2e9948ded
Adithyanvp5 <adithyanvp230@gmail.com>
2026-03-29 20:59:13 +0530

    Add CNN model creation function in cnn_model.py

---
+++ src/cnn_model.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+
+def create_model(input_shape):
+    model = Sequential([
+        Conv2D(32, (3,3), activation='relu', input_shape=input_shape),
+        MaxPooling2D((2,2)),
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPooling2D((2,2)),
+        Flatten(),
+        Dense(64, activation='relu'),
+        Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mse')
+    return model
